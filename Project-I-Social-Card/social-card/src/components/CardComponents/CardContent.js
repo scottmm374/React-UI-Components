99ceadf0fdaaa4f4a8988d780b8fd59adf76b46f
Michelle Scott <scottmm374@gmail.com>
2019-05-29 19:54:35 -0400

    added compoenets in Imgthumb, CardBanner, CardContent

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+function CardContent() {
+    return <div>
+        <h4>Get Started with React</h4>
+        <p>React makes it painless to create interactive UIs. Design simple veiws for 
+            each state in your application.
+        </p>
+        <a href="#">reactjs.org</a>
+    </div>
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+
+function HeaderTitle() {
+    <h1>Lambda School </h1>
+}
+
+export default HeaderTitle;
