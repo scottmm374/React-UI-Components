6d379f220de0d48110da665c362105d87e7bd37c
Michelle Scott <scottmm374@gmail.com>
2019-05-29 20:30:16 -0400

    CardContainer complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
+function CardContainer() {
+    return (
+        <div className="CardContainer">
+            <CardBanner />
+            <CardContent />
+        </div>
+    );
+}
+
+export default CardContainer;
 
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,13 +3,15 @@ import './Card.css';
 
 
 function CardContent() {
-    return <div>
+    return (
+    <div>
         <h4>Get Started with React</h4>
         <p>React makes it painless to create interactive UIs. Design simple veiws for 
             each state in your application.
         </p>
         <a href="#">reactjs.org</a>
     </div>
+    );
 }
 
 export default CardContent;
